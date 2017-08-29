fe48a7a555035940953925b4828e8980800ab8fb
Michael <shawm16@live.com>
2017-08-29 12:39:58 -0700

    uses a array

--- Problem_2.go
+++ Problem_2.go
@@ -6,15 +6,21 @@ func main(){
   var (
     limit = 4000000
     sum = 0
-    a = 1
-    b = 1
-    c = a + b
   )
-  for c < limit {
-    sum += c
-    a = b + c
-    b = c + a
-    c = a + b
+  /* This is a example of defining and using a array instead
+   of 3 different variables you have x[0-2] 0, 1, 2, = 3 variables*/
+  var x [3]int
+  x[0] = 1
+  x[1] = 1
+  x[2] = x[0] + x[1]
+
+
+  /*in Go the for statement is the while statement */
+  for x[2] < limit {
+    sum += x[2]
+    x[0] = x[1] + x[2]
+    x[1] = x[2] + x[0]
+    x[2] = x[0] + x[1]
   }
   fmt.Println(sum)
 
