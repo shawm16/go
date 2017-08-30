a5ae668170e472aaab40432a3ec42c779f01667c
Michael <shawm16@live.com>
2017-08-30 10:29:02 -0700

    Updated comments

--- Problem_9.go
+++ Problem_9.go
@@ -1,7 +1,9 @@
 package main
 
+/* This is a way to import more than one package */
 import (
   "fmt"
+  /*This is the math package that we need for math.Sqrt() */
   "math"
 )
 
@@ -9,9 +11,12 @@ func main(){
   var(
     boolean = 1
   )
+  /*The reason we define the variables as a float 64 is because math.Sqrt(FLOAT64) */
   var a float64 = 0
   var b float64 = 0
+/*the boolean variable is just a true or false checker*/
   for boolean != 0 {
+/*This is where the algorithm is put into play. Thanks Mar. */
     for a = 1; a < 1000; a++ {
       if a + b + math.Sqrt(a*a+b*b) == 1000{
         if (a*a) + (b*b) == ((math.Sqrt(a*a+b*b))*(math.Sqrt(a*a+b*b))) && a < b && b < math.Sqrt(a*a+b*b) {
